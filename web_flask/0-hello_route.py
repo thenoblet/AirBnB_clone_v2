d0653767e10d6c242278fb5656db80430e23a4b9
noblet <appiahpatricknoblet@gmail.com>
2024-05-19 10:40:17 +0100

    Added a simple flask app{

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+flask_app.py
+
+This module sets up a basic Flask web application that responds with "Hello HBNB" when accessed.
+
+Imports:
+    Flask from the flask package.
+
+Functions:
+    home(): Returns a welcome message "Hello HBNB".
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def home():
+    """
+    The home function handles requests to the root URL ('/') and returns a simple greeting.
+
+    Returns:
+        str: A welcome message "Hello HBNB".
+    """
+    return "Hello HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
